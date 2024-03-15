8761e611bd681c38f77ae7f3c26f9ac2c3ccdf61
deshinta <deshintaaulia065@gmail.com>
2024-03-15 17:16:18 +0700

    feat: add register and find user by req user id

--- controller/controllerUser.js
+++ controller/controllerUser.js
@@ -1,9 +1,32 @@
+const User = require("../models/user")
+
 class ControllerUser {
     static async register(req,res,next){
         try {
-            
+            let { name, email, username, password } = req.body
+
+            let newUser = await User.create({ name, email, username, password })
+
+            res.status(201).json(newUser)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async findUser (req,res,next){
+        try {
+            let user = await User.findByPk(req.user.id)
+
+            res.status(200).json({
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email
+            })
         } catch (error) {
             next(error)
         }
     }
 }
+
+module.exports = ControllerUser
