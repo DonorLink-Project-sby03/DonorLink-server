d4a8cb18401019d6e40152c64411a83de4e1b797
khaerulwalid <ckrwislam@gmail.com>
2024-03-15 17:25:59 +0700

    fix schema

--- migrations/20240315102439-addStockToDonor.js
+++ migrations/20240315102439-addStockToDonor.js
@@ -9,7 +9,7 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.removeColumn('Donors', 'bloodType', {})
+    await queryInterface.addColumn('Donors', 'stock', Sequelize.INTEGER)
   },
 
   async down (queryInterface, Sequelize) {
@@ -19,6 +19,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.addColumn('Donors', 'bloodType', Sequelize.STRING)
+    await queryInterface.removeColumn('Donors', 'stock', {})
   }
 };
