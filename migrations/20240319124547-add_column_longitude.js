2955ecc4e183cc4be6b9ac23e03957ad957a48ff
khaerulwalid <ckrwislam@gmail.com>
2024-03-19 20:18:32 +0700

    feature add latitude and longitude to db

---
+++ migrations/20240319124547-add_column_longitude.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Recipients', 'latitude', Sequelize.STRING)
+    await queryInterface.addColumn('Recipients', 'longitude', Sequelize.STRING)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeColumn('Recipients', 'latitude', {})
+    await queryInterface.removeColumn('Recipients', 'longitude', {})
+  }
+};
--- models/recipient.js
+++ models/recipient.js
@@ -42,6 +42,8 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     image: DataTypes.STRING,
+    latitude: DataTypes.STRING,
+    longitude: DataTypes.STRING,
     bloodType: {
       type: DataTypes.STRING,
       allowNull: false,
